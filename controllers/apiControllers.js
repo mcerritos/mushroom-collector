b12443320b34f929b3c6c09e0ca2cbef547de654
Melody <leclecticat@gmail.com>
2020-06-04 14:03:47 -0700

    adds api controllers for log functionality

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -7,3 +7,29 @@ app.use(bodyParser.json());
 // api call to add item to log 
 // api call to get information for profile
 // api call to get items for basket??
+
+const addLog = (req, res) => {
+    db.User.findById(req.params.UserId, (err, currentUser) => {
+        if (err) {
+            return res.status(400).json({status: 400, error: 'Current user not found.'});
+        }
+
+        currentUser.log.push(req.params.mushroomId)
+    })
+}
+
+const getLog = (req, res) => {
+    db.User.findById(req.params.UserId, (err, currentUser) => {
+        if (err) {
+            return res.status(400).json({status: 400, error: 'Current user not found.'});
+        }
+
+        const currentLog = currentUser.log ;
+        res.json(currentLog);
+    })
+}
+
+module.exports = {
+    addLog,
+    getLog,
+}
